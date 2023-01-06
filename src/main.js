99f6fa16c5443a71d8eb00e063e033986f0e58ba
Paul Degemann <paul.degemann@swk.de>
2023-01-06 21:01:05 +0100

    fixed to high viewport on mobile devices

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import viewportFix from "./global-js/viewportFix";
 import "./assets/scss/main.scss";
+
+viewportFix();
 
 createApp(App).mount("#app");
